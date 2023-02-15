eea1fab15f11ea1eafa75247cf4fba7665b59c3b
Billie <billie.dorries28@gmail.com>
2023-02-15 12:16:35 -0600

    completed inheritance exercises

---
+++ src/main/java/shapes/Square.java
@@ -0,0 +1,19 @@
+package shapes;
+
+public class Square extends Rectangle{
+
+    protected double side;
+    public Square(double side){
+        super(side,side);
+        this.side = side;
+    }
+
+    @Override
+    public double getPerimeter(){
+        return side*4;
+    }
+    @Override
+    public double getArea(){
+        return side*side;
+    }
+}
